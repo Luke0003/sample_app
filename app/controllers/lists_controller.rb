2c02ee04c767dc82b3353229cf45f2e4cd94a900
EC2 Default User <ec2-user@ip-172-31-15-106.ap-northeast-1.compute.internal>
2022-09-27 04:54:15 +0000

    [Add]環境変数の利用

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -9,33 +9,34 @@ class ListsController < ApplicationController
       redirect_to list_path(@list.id)
     else
       render :new
-    end   
+    end
   end
 
   def index
     @lists = List.all
+    puts "作成したキー#{ENV['SECRET_KEY']}"
   end
 
   def show
-     @list = List.find(params[:id])  
+     @list = List.find(params[:id])
   end
 
   def edit
     @list = List.find(params[:id])
   end
-  
+
   def update
     list = List.find(params[:id])
     list.update(list_params)
-    redirect_to list_path(list.id)  
+    redirect_to list_path(list.id)
   end
-  
+
   def destroy
     list = List.find(params[:id])
     list.destroy
     redirect_to "/lists"
   end
-  
+
   private
     def list_params
       params.require(:list).permit(:title, :body, :image)
